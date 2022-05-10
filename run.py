ac241661d7d6b1ae57f0a7a82e37cc343e331de5
MartyJan <jan.marty@gmail.com>
2022-05-11 01:39:04 +0800

    remove app.run()

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 """ Main program """
 
 from create_app import create_app
+app = create_app()
 
+""" Local Testing """
+"""
 if __name__ == "__main__":
     app = create_app()
     with app.app_context():
         app.run()        # Activate server
+"""
